521e02d5bf4f09eff73d7dc49e98e89479b03179
KHAI NGUYEN <kqn001@bucknell.edu>
2018-12-02 12:35:50 -0800

    done

--- script.js
+++ script.js
@@ -5,22 +5,27 @@ $(document).ready(function () {
     });
     $('.frame').click(function () {
         let frames = document.getElementsByClassName('frame')
+        let subs = document.getElementsByClassName('sub')
+
         if (this.classList.contains('active')) {
             this.classList.remove('active')
             for (let i = 0; i < frames.length; i++) {
                 frames[i].classList.remove('active')
                 frames[i].style.opacity = '1'
+                subs[i].classList.remove('active')
             }
             $('#color-overlay').css('background', 'white')
         } else {
             for (let i = 0; i < frames.length; i++) {
                 frames[i].classList.remove('active')
                 frames[i].style.opacity = '0.4'
+                subs[i].classList.remove('active')
             }
-
             var t = this;
             setTimeout(function () {
                 $(t).addClass('active')
+                $(t).children().addClass('active')
+                $(t).siblings().addClass('active')
                 // $('#color-overlay').css('background', t.dataset.complementary)    
             }, 150);
         }
